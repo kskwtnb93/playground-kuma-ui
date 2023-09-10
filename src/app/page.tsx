464eb99890d3438fae4ce091eab5a480ba74b65f
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-10 22:04:08 +0900

    🚧 Try CSS API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import { css } from "@kuma-ui/core";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      className={css`
+        flex: 1;
+      `}
+    >
+      <p
+        className={css`
+          // CSS API の補完サポートはこれからとのこと（https://www.kuma-ui.com/docs/API/css）
+          text-align: center;
+          font-size: 5em;
+          font-weight: bold;
+        `}
+      >
+        Center Text
+      </p>
+    </main>
+  );
 }
